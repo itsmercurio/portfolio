b2bdcfecb4f36bb7dcd13bdc104097fe051bcdb8
Alexis Mansilla Montull <lolalexis.am@gmail.com>
2025-05-29 20:11:08 +0200

    Fix: No css

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\/(.*)/g, '\1')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+} else {
+  assetPrefix = '/portfolio/'
+  basePath = '/portfolio'
+}
+
 const nextConfig = {
   output: 'export',
-  basePath: '/portfolio',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
     unoptimized: true,
   },
