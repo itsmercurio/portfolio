031bee46f6fcc7d36e6de73e305aa836f47d103a
Alexis Mansilla Montull <lolalexis.am@gmail.com>
2025-05-29 20:27:17 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -1,26 +1,15 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = ''
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\/(.*)/g, '\1')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-} else {
-  assetPrefix = '/portfolio/'
-  basePath = '/portfolio'
-}
-
 const nextConfig = {
   output: 'export',
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
+  experimental: {
+    appDir: true
+  }
 }
 
 module.exports = nextConfig
